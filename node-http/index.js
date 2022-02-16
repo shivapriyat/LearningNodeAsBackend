f08bb27e4307286346a7fa2ac10d6b32f6f15fcb
sthanumu <shivapriya.thanumurthy@oracle.com>
2022-02-16 23:57:08 +0530

    Node HTTP Example 2

--- node-http/index.js
+++ node-http/index.js
@@ -1,15 +1,55 @@
-const http=require("http");
-const hostname="localhost";
-const port=3000;
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const hostname = "localhost";
+const port = 3000;
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
 
-    console.log(req.headers);
-    res.setHeader("Content-Type","text/html");
-    res.statusCode=200;
-    res.write("<html><body>Hello world from nodejs </body></html");
-    res.end();
+    console.log("Request for url: " + req.url + " method: " + req.method);
+    if (req.method === "GET") {
+        let fileUrl;
+        if (req.url === "/") {
+            fileUrl = "/index.html";
+        }
+        else {
+            fileUrl = req.url;
+        }
+        let filePath = path.resolve("public" + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt == ".html") {
+            let exists = fs.existsSync(filePath);
+            if (exists) {
+                res.setHeader("Content-Type", "text/html");
+                res.statusCode = 200;
+                fs.createReadStream(filePath).pipe(res);
+            }
+            else {
+                res.statusCode = 404;
+                res.setHeader('Content-Type', 'text/html');
+                res.end('<html><body><h1>Error 404: ' + fileUrl +
+                    ' not found</h1></body></html>');
+                return;
+            }
+        }
+        else {
+            //invalid file ext
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: ' + fileUrl +
+                ' not a HTML file</h1></body></html>');
+        }
+
+    }
+    else {
+        //invalid http method
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + req.method +
+            ' not supported</h1></body></html>');
+    }
+
 });
-server.listen(port, hostname, ()=> {
+server.listen(port, hostname, () => {
     console.log(`server running at http://${hostname}:${port}/`);
 })
