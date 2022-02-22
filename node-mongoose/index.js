d1669c3b58c1c263db90916d98588f873f251816
sthanumu <shivapriya.thanumurthy@oracle.com>
2022-02-22 23:08:29 +0530

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,28 +8,25 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
 
     console.log('Connected correctly to server');
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
+   
 });
