88fdc0caa50a04d37942b808ba2a7da4ac94dc1a
sthanumu <shivapriya.thanumurthy@oracle.com>
2022-02-22 23:14:41 +0530

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -14,6 +14,27 @@ connect.then((db) => {
     })
     .then((dish) => {
         console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
         
         return Dishes.find({}).exec();
     })
