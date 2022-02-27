b76063ca4949b406861346878a44a2f6c04dba35
sthanumu <shivapriya.thanumurthy@oracle.com>
2022-02-27 22:44:30 +0530

    Basic Authentication

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -30,6 +30,33 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req,res,next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if(!authHeader) {
+    var err = new Error("Un authenticated ");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+    return;
+  }
+  let authArr = new Buffer.from(authHeader.split(" ")[1],"base64").toString().split(":");
+  let username = authArr[0];
+  let password =authArr[1];
+  if(username==="admin" && password=="password") {
+    next();
+  }
+  else {
+    console.log(username);
+    console.log(password);
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');      
+    err.status = 401;
+    next(err);
+  }
+}
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
